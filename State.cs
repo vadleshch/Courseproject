c7d8d19b6d7e55083b4016094dc7edf7d64b8e79
vladle <165798388+vadleshch@users.noreply.github.com>
2026-04-19 18:54:37 +0300

    екземпляр цього класу буде містити стан

---
+++ State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Courseproject
+{
+    internal class State
+    {
+        public uint[] A  = new uint[5];
+        public uint[] B  = new uint[11];
+        public uint R1;
+        public uint R2;
+        public uint L1;
+        public uint L2;
+    }
+}
